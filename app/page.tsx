03e535222651b76791566991959dfb5fede3794c
Peter Eriksson <tuvoc1@hotmail.com>
2026-09-25 15:47:14 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -21,6 +21,7 @@ export default async function HomePage() {
         Exercises for{" "}
         <span className=" text-main ">intermediate language learners</span>
       </h2>
+      <h3>hello</h3>
 
       {session ? <LanguageSelect /> : <Login />}
 
